2a2a3cfa1b84d737b630ea6d97232b73c3210060
Harrison Hall <harryson.hall@gmail.com>
2025-06-11 21:49:36 -0400

    Add TURN servers for better WebRTC NAT traversal

--- src/components/video-chat.jsx
+++ src/components/video-chat.jsx
@@ -30,6 +30,18 @@ export function VideoChat({ isOpen, onClose, callingUserId }) {
     iceServers: [
       { urls: 'stun:stun.l.google.com:19302' },
       { urls: 'stun:stun1.l.google.com:19302' },
+      { urls: 'stun:stun2.l.google.com:19302' },
+      // Public TURN servers for NAT traversal
+      {
+        urls: 'turn:openrelay.metered.ca:80',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      },
+      {
+        urls: 'turn:openrelay.metered.ca:443',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      }
     ]
   }
 
